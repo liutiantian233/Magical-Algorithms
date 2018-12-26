e07e20126fe1ae120c0587bf00b7ffd768bc88e5
TianTian <44875235+liutiantian233@users.noreply.github.com>
2018-12-26 11:29:49 -0500

    Update main2.c

--- Sort/Bubble Sort/main2.c
+++ Sort/Bubble Sort/main2.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+struct student
+{
+  char name[21];
+  char score;
+};
+
+int main()
+{
+  struct student a[100],t;
+  int i,j,n;
+  scanf("%d",&n);
+  
+  for(i=1;i<=n;i++)
+    scanf("%s %d",a[i].name,&a[i].score);
+  
+  for(i=1;i<=n-1;i++)
+  {
+    for(j=1;j<=n-i;j++)
+    {
+      if(a[j].score<a[j+1].score)
+      {
+        t=a[j];
+        a[j]=a[j+1];
+        a[j+1]=t;
+      }
+    }
+  }
+  for(i=1;i<=n;i++)
+    printf("%s\n",a[i].name);
+  
+  getchar();getchar();
+  return 0;
+}
