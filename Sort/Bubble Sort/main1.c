a7f9d05afa44d10b698001859ead605e1210d005
TianTian <44875235+liutiantian233@users.noreply.github.com>
2018-12-26 11:24:01 -0500

    Update main1.c

--- Sort/Bubble Sort/main1.c
+++ Sort/Bubble Sort/main1.c
@@ -1,1 +1,28 @@
-
+#include <stdio.h>
+int main()
+{
+  int a[100],i,j,t,n;
+  
+  scanf("%d",&n);
+  
+  for(i=1;i<=n;i++)
+    scanf("%d",&a[i]);
+  
+  for(i=1;i<=n-1;i++)
+  {
+    for(j=1;j<=n-i;j++)
+    {
+      if(a[j]<a[j+1])
+      {
+        t=a[j];
+        a[j]=a[j+1];
+        a[j+1]=t;
+      }
+    }
+  }
+  for(i=1;i<=n;i++)
+    printf("%d ",a[i]);
+  
+  getchar();getchar();
+  return 0;
+}
