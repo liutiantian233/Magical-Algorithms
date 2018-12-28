a3280a55a51baabb75918b7243686458b9522417
TianTian <44875235+liutiantian233@users.noreply.github.com>
2018-12-28 12:31:13 -0500

    Update main1.c

--- Sort/Summary of Sorting Algorithms/main1.c
+++ Sort/Summary of Sorting Algorithms/main1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main()
+{
+  int a[1001],n,i,t;
+  
+  for(i=1;i<=1000;i++)
+    a[i]=0;
+  
+  scanf("%d",&n);
+  
+  for(i=1;i<=n;i++)
+  {
+    scanf("%d",&t);
+    a[t]=1;
+  }
+  
+  for(i=1;i<=1000;i++)
+  {
+    if(a[i]==1)
+      printf("%d ",i);
+  }
+  
+  getchar();getchar();
+  return 0;
+}
